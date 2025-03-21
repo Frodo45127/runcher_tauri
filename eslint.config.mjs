9579a723a94124fd7d836f6918754c687ceb1b5e
Frodo45127 <frodo_gv@hotmail.com>
2025-03-21 13:45:03 +0100

    Added ESLint to help with the typescript linting and docs.

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import jsdoc from 'eslint-plugin-jsdoc';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  jsdoc.configs['flat/recommended'],
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      jsdoc,
+    },
+    rules: {
+      'jsdoc/require-description': 'warn'
+    }
+  }
+);
